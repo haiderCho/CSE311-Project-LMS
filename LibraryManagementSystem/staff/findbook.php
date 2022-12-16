edaa4300b39394133f357c3c67305f4f49c185ff
Haider <117144478+haiderCho@users.noreply.github.com>
2022-12-16 17:01:58 +0600

    Staff Pages Update

--- LibraryManagementSystem/staff/findbook.php
+++ LibraryManagementSystem/staff/findbook.php
@@ -59,6 +59,7 @@ if ($_SESSION['Student_ID']) {
                                 </li>
                                 <li><a href="book.php"><i class="menu-icon icon-book"></i>All Books </a></li>
                                 <li><a href="history.php"><i class="menu-icon icon-tasks"></i>Previously Borrowed Books </a></li>
+                                <li><a href="recommendations.php"><i class="menu-icon icon-list"></i>Recommend Books </a></li>
                                 <li><a href="current.php"><i class="menu-icon icon-list"></i>Currently Issued Books </a></li>
                             </ul>
                             <ul class="widget widget-menu unstyled">
@@ -68,17 +69,44 @@ if ($_SESSION['Student_ID']) {
                         <!--/.sidebar-->
                     </div>
                     <!--/.span3-->
+                    
+                    <div class="span9">
+
+                        <div class="content">
+                            <div class="btn-controls">
+                                <div class="btn-box-row row-fluid">
+                                    <a href="findbook.php" class="btn-box big span4"><i class=" icon-search"></i><b>Find Book</b>
+                                    </a>
+                                </div>
+                            </div>
+                            <!--/.module-->
+                        </div>
+                        
+                        <!--/.content-->
+                    </div>
+                
+                    <!--/.span9-->
                 </div>
             </div>
             <!--/.container-->
         </div>
-        <div class="footer">
+<div class="footer">
             <div class="container">
                 <b class="copyright">&copy; 2022 Ohara Library Management System. </b>All rights reserved.
             </div>
         </div>
+        
         <!--/.wrapper-->
+        <script src="scripts/jquery-1.9.1.min.js" type="text/javascript"></script>
+        <script src="scripts/jquery-ui-1.10.1.custom.min.js" type="text/javascript"></script>
+        <script src="bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
+        <script src="scripts/flot/jquery.flot.js" type="text/javascript"></script>
+        <script src="scripts/flot/jquery.flot.resize.js" type="text/javascript"></script>
+        <script src="scripts/datatables/jquery.dataTables.js" type="text/javascript"></script>
+        <script src="scripts/common.js" type="text/javascript"></script>
+      
     </body>
+
 </html>
 
 <?php }
