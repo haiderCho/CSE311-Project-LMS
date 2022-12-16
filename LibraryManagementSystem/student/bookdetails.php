0c7062af08661b0a8c6cb6d4c4bb2ab500514c8a
iSabbirHossain <118951330+iSabbirHossain@users.noreply.github.com>
2022-12-16 17:57:25 +0000

    update

--- LibraryManagementSystem/student/bookdetails.php
+++ LibraryManagementSystem/student/bookdetails.php
@@ -68,7 +68,58 @@ if ($_SESSION['Personal_ID']) {
                         <!--/.sidebar-->
                     </div>
                     <!--/.span3-->
-                                        
+                    
+                    <div class="span9">
+                        <div class="content">
+
+                        <div class="module">
+                            <div class="module-head">
+                                <h3>Book Details</h3>
+                            </div>
+                            <div class="module-body">
+                        <?php
+                            $x=$_GET['id'];
+                            $sql="select * from LMS.book where BookId='$x'";
+                            $result=$conn->query($sql);
+                            $row=$result->fetch_assoc();    
+                            
+                                $bookid=$row['BookId'];
+                                $name=$row['Title'];
+                                $Genre=$row['Genre'];
+                                $year=$row['Year'];
+                                $avail=$row['Availability'];
+                                echo "<b>Book ID:</b> ".$bookid."<br><br>";
+                                echo "<b>Title:</b> ".$name."<br><br>";
+                                $sql1="select * from LMS.author where BookId='$bookid'";
+                                $result=$conn->query($sql1);
+                                
+                                echo "<b>Author:</b> ";
+                                while($row1=$result->fetch_assoc())
+                                {
+                                    echo $row1['Author']."&nbsp;";
+                                }
+                                echo "<br><br>";
+                                echo "<b>Genre:</b> ".$Genre."<br><br>";
+                                echo "<b>Year:</b> ".$year."<br><br>";
+                                echo "<b>Availability:</b> ".$avail."<br><br>";
+
+                                
+                        
+                           
+                            ?>
+                            
+                        <a href="book.php" class="btn btn-primary">Go Back</a>                             
+                               </div>
+                           </div>
+                            </div>
+                    <!--/.span3-->
+                    <!--/.span9-->
+                
+                    <!--/.span3-->
+                    <!--/.span9-->
+                </div>
+                    
+                    <!--/.span9-->
                 </div>
                     <!--/.span9-->
                 </div>
@@ -79,7 +130,18 @@ if ($_SESSION['Personal_ID']) {
                 <b class="copyright">&copy; 2022 Ohara Library Management System. </b>All rights reserved.
             </div>
         </div>
+        
+        <!--/.wrapper-->
+        <script src="scripts/jquery-1.9.1.min.js" type="text/javascript"></script>
+        <script src="scripts/jquery-ui-1.10.1.custom.min.js" type="text/javascript"></script>
+        <script src="bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
+        <script src="scripts/flot/jquery.flot.js" type="text/javascript"></script>
+        <script src="scripts/flot/jquery.flot.resize.js" type="text/javascript"></script>
+        <script src="scripts/datatables/jquery.dataTables.js" type="text/javascript"></script>
+        <script src="scripts/common.js" type="text/javascript"></script>
+      
     </body>
+
 </html>
 
 <?php }
--- LibraryManagementSystem/student/index.php
+++ LibraryManagementSystem/student/index.php
@@ -65,6 +65,41 @@ require('dbconn.php');
                         <!--/.sidebar-->
                     </div>
                     <!--/.span3-->
+                    <div class="span9">
+                    	<center>
+                           	<div class="card" style="width: 50%;"> 
+                    			<img class="card-img-top" src="images/profile2.png" alt="Card image cap">
+                    			<div class="card-body">
+
+                                <?php
+                                $Personal_ID = $_SESSION['Personal_ID'];
+                                $sql="select * from LMS.user where Personal_ID='$Personal_ID'";
+                                $result=$conn->query($sql);
+                                $row=$result->fetch_assoc();
+
+                                $name=$row['Name'];
+                                $Type=$row['Type'];
+                                $email=$row['EmailId'];
+                                $mobno=$row['MobNo'];
+                                ?> 
+                    				<h1 class="card-title"><center><?php echo $name ?></center></h1>
+                    				<br>
+                    				<p><b>Email ID: </b><?php echo $email ?></p>
+                    				<br>
+                    				<p><b>Personal ID: </B><?php echo $Personal_ID ?></p>
+                    				<br>
+                    				<p><b>Type: </b><?php echo $Type ?></p>
+                    				<br>
+                    				<p><b>Mobile number: </b><?php echo $mobno ?></p>
+                    				</b>
+                    			</div>
+                    		</div>
+                            <br>
+                            <a href="edit_student_details.php" class="btn btn-primary">Edit Details</a>    
+      					</center>              	
+                    </div>
+                    
+                    <!--/.span9-->
                 </div>
             </div>
             <!--/.container-->
@@ -74,6 +109,16 @@ require('dbconn.php');
                 <b class="copyright">&copy; 2022 Ohara Library Management System. </b>All rights reserved.
             </div>
         </div>
+        
         <!--/.wrapper-->
+        <script src="scripts/jquery-1.9.1.min.js" type="text/javascript"></script>
+        <script src="scripts/jquery-ui-1.10.1.custom.min.js" type="text/javascript"></script>
+        <script src="bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
+        <script src="scripts/flot/jquery.flot.js" type="text/javascript"></script>
+        <script src="scripts/flot/jquery.flot.resize.js" type="text/javascript"></script>
+        <script src="scripts/datatables/jquery.dataTables.js" type="text/javascript"></script>
+        <script src="scripts/common.js" type="text/javascript"></script>
+      
     </body>
+
 </html>
